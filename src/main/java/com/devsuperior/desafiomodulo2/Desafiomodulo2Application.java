35247a1322f4d2397e3e0d121554d970bd1a5bc7
Renata Viotto Hassum <renatahassum@gmail.com>
2022-08-11 09:03:14 -0300

    DESAFIO: Modelo de domínio e ORM

---
+++ src/main/java/com/devsuperior/desafiomodulo2/Desafiomodulo2Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiomodulo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafiomodulo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafiomodulo2Application.class, args);
+	}
+
+}
